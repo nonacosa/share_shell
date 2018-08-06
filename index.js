1c9d62cfc8013b3a0addba4738af4c104ae8d18a
six.nonacosa <pkwenda@163.com>
2018-08-07 00:28:25 +0800

    add demo test shell.exec

---
+++ index.js
@@ -0,0 +1,14 @@
+var shell = require('shelljs');
+
+if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git');
+    shell.exit(1);
+}
+shell.exec('node --version', function (code, stdout, stderr) {
+    if (code === 0) {
+        console.log(stdout)
+    } else {
+        console.log(stderr)
+    }
+});
+
