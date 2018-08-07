af13473f25f7e45c6221501892562ac8add0a1ae
six.nonacosa <pkwenda@163.com>
2018-08-08 03:02:59 +0800

    del annocation

--- bin/file.js
+++ bin/file.js
@@ -1,11 +1,10 @@
 var fs = require('fs-extra')
 var format = require('./format')
 
-// fs.mkdirs('/Users/zhuang/Desktop/test.html')
 
 // With async/await:
 async function mkfile(str, path) {
-    // let f = '/Users/zhuang/Desktop/test2.html'
+
     try {
 
         await fs.outputFile(path, format.toHtmlFormat(str))
