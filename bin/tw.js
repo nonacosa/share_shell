5fe2bfa1d6d60edb310301d9e9045e328ca59239
six.nonacosa <pkwenda@163.com>
2018-08-08 03:02:23 +0800

    del annocation

--- bin/tw.js
+++ bin/tw.js
@@ -9,24 +9,18 @@ var file = require('./file');
 
 program
     .version(appInfo.version)
-// .usage('这里是我私人玩耍的命令哦！[options] <package>')
-
-//像git风格一样的子命令
 program
-    //子命令
     .command('share <shell>')
-    //短命令 - 简写方式
+
     .alias('s')
-    //说明
+
     .description('Enter the "shell" you want to convert and include it in \" \"  ')
-    //resume的子命令
+
     .option("-p, --path <path>", "Enter you html path , default ./share_you_shell.html")
-    //注册一个callback函数
+
     .action(function (cmd, options) {
         var path = typeof options.path == 'string' ? options.path : "./share_you_shell.html"
 
-        console.log('cmd, path');
-        console.log(cmd, path);
         exec_shell.exec(cmd, (res) => {
 
             var res_arr = S(res).lines()
@@ -34,7 +28,7 @@ program
             file.mkfile(str, path)
         })
     }).on('--help', function () {
-        //这里输出子命令的帮助
+
         console.log('  Examples:');
     });
 
